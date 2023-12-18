ccd2866630e0fd76f0164c0d6d720f2d42340f60
Ismar Slomic <ismar@slomic.no>
2023-12-18 20:43:17 +0100

    test(cpu): no need to patch platform since all platforms are supported

--- tests/rpi/cpu/test_cpu.py
+++ tests/rpi/cpu/test_cpu.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 """Tests to verify the RPI CPU usage readings"""
 
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 
 import psutil
 
 from rpi.cpu.cpu import read_cpu_use_percent
 
 
-# patching platform since psutil.cpu_percent is not available for all platforms
-@patch("sys.platform", "linux")
 def test_read_cpu_use_percent():
     # Mock psutil
     psutil.cpu_percent = MagicMock(return_value=0.2)
