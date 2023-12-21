76b0a82194614e9386aec472d061ff56f7395f2b
Ismar Slomic <ismar@slomic.no>
2023-12-21 22:57:54 +0100

    chore(model): rename the test case

--- tests/rpi/model/test_model.py
+++ tests/rpi/model/test_model.py
@@ -8,7 +8,7 @@ from rpi.model.model import read_rpi_model
 
 # noinspection PyUnusedLocal
 @patch("builtins.open", new_callable=mock_open, read_data="\x00Raspberry Pi 5 Model B Rev 1.0\x00")
-def test_read_memory_use(mock_file):
+def test_read_rpi_model_and_strip_specific_characters(mock_file):
     # Call function
     model: str = read_rpi_model()
 
