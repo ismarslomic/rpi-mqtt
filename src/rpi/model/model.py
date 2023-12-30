88b77a9df7c81327ef3841315ecb84f3bc013171
Ismar Slomic <ismar@slomic.no>
2023-12-31 00:18:45 +0100

    feat(os): add service for reading number of available updates

--- src/rpi/model/model.py
+++ src/rpi/model/model.py
@@ -6,7 +6,7 @@ def read_rpi_model() -> str:
     """Read Rpi model"""
 
     model_file_name = "/sys/firmware/devicetree/base/model"
-    with open(model_file_name, "r") as f:
+    with open(model_file_name, "r", encoding="utf-8") as f:
         model = f.readline().strip("\x00")
 
     return model
--- src/rpi/os/os.py
+++ src/rpi/os/os.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 """Service for reading the Rpi OS info"""
+
 import subprocess
+
+# Apt is not available on Mac
+APT_AVAILABLE = True
+try:
+    # noinspection PyUnresolvedReferences
+    import apt
+except ImportError:
+    APT_AVAILABLE = False
 
 
 def read_rpi_os_kernel() -> str:
@@ -20,7 +29,26 @@ def read_os_release() -> str:
     """Read OS release"""
 
     release_file_name = "/etc/os-release"
-    with open(release_file_name, "r") as f:
+    with open(release_file_name, "r", encoding="utf-8") as f:
         release = f.readline().replace("PRETTY_NAME=", "").replace("\n", "").replace('"', "").strip()
 
     return release
+
+
+def read_number_of_available_updates() -> int:
+    """Read number of available package updates (using apt), returns -1 if apt not available"""
+
+    if APT_AVAILABLE:
+        cache = apt.Cache()
+        cache.open(None)
+        # apt update will be run automatically every day by the OS, so at some point of time the upgrade will report
+        # available updates. We could run cache.update(), but this command requires sudo
+        cache.upgrade()
+        # Get marked changes
+        changes = cache.get_changes()
+
+        # Return number of changes
+        return len(changes)
+
+    # apt not available
+    return -1
