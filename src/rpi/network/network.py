11f8c3d917c5a1ba4857dc858b0b38908606876a
Ismar Slomic <ismar@slomic.no>
2024-01-14 23:15:56 +0100

    fix(network): convert hex to int

--- src/rpi/network/network.py
+++ src/rpi/network/network.py
@@ -14,26 +14,8 @@ def read_container_host_ip() -> str:
 
     ip_pipe_file_name = "/app/host/host_ip_pipe"
     with open(ip_pipe_file_name, "r", encoding="utf-8") as f:
-        data = f.readline().strip("\x00")
-        ip = ""
-        for line in data.split("\n"):
-            #
-            # Example:
-            # 3: 8D01A8C0:0016 B801A8C0:E3A7 01 00000000:00000000 02:00096E3C 00000000
-            # |   |                           |--> status: connection state
-            # |   |
-            # |   |
-            # |   |
-            # |   |---------------------------> addr: local IPv4 address
-            # |----------------------------------> id
-            #
-            mo = re.match("^.{2}(?P<id>.{2}).{2}(?P<addr>.{8})..{4} .{8}..{4} (?P<status>.{2}).*|", line)
-            if mo and mo.group("id") != "sl":
-                status = int(mo.group("status"), 16)  # convert from hex to decimal
-                if status == 1:  # connection established
-                    ip = _little_endian_hex_to_ip(mo.group("addr"))
-                    break
-        return ip
+        tcp_content = f.readline().strip("\x00")
+        return _parse_ip_from_tcp_content(tcp_content)
 
 
 def read_container_host_hostname() -> str:
@@ -72,8 +54,31 @@ def read_wifi_connection() -> WiFiConnectionInfo:
     return WiFiConnectionInfo(ssid=ssid, signal_strength_dbm=signal)
 
 
-def _little_endian_hex_to_ip(little_endian_hex) -> str:
+def _parse_ip_from_tcp_content(content: str) -> str:
+    ip: str = ""
+
+    for line in content.split("\n"):
+        #
+        # Example:
+        # 3: 8D01A8C0:0016 B801A8C0:E3A7 01 00000000:00000000 02:00096E3C 00000000
+        # |   |                           |--> status: connection state
+        # |   |
+        # |   |
+        # |   |
+        # |   |---------------------------> addr: local IPv4 address
+        # |----------------------------------> id
+        #
+        mo = re.match("^.{2}(?P<id>.{2}).{2}(?P<addr>.{8})..{4} .{8}..{4} (?P<status>.{2}).*|", line, re.MULTILINE)
+        if mo and mo.group("id") != "sl":
+            status = int(mo.group("status"), 16)  # convert from hex to decimal
+            if status == 1:  # connection established
+                ip = _little_endian_hex_to_ip(mo.group("addr"))
+                break
+    return ip
+
+
+def _little_endian_hex_to_ip(little_endian_hex: str) -> str:
     """Converts IP address in little-endian four-byte hexadecimal number to IP string"""
 
     # https://stackoverflow.com/a/2198052
-    return socket.inet_ntoa(struct.pack("<L", little_endian_hex))
+    return socket.inet_ntoa(struct.pack("<L", int(little_endian_hex, 16)))
--- tests/rpi/network/test_network.py
+++ tests/rpi/network/test_network.py
@@ -3,7 +3,7 @@
 
 from unittest.mock import MagicMock, patch
 
-from rpi.network.network import read_wifi_connection
+from rpi.network.network import _parse_ip_from_tcp_content, read_wifi_connection
 from rpi.network.types import WiFiConnectionInfo
 
 
@@ -49,3 +49,20 @@ def test_read_wifi_connection_when_disconnected(mock_run):
     # Assert Wi-Fi information returned
     assert "" == wifi_info.ssid
     assert 0 == wifi_info.signal_strength_dbm
+
+
+def test_parse_ip_from_tcp_content():
+    tcp_content: str = (
+        "  sl  local_address rem_address   st tx_queue rx_queue tr tm->when retrnsmt   uid  timeout inode\n"
+        "   0: 0100007F:0277 00000000:0000 0A 00000000:00000000 00:00000000 00000000     0        0 40517 1 00000000409e6c91 100 0 0 10 0\n"
+        "   1: 00000000:170C 00000000:0000 0A 00000000:00000000 00:00000000 00000000  1000        0 17867 1 000000001cdccca5 100 0 0 10 0\n"
+        "   2: 00000000:075B 00000000:0000 0A 00000000:00000000 00:00000000 00000000  1883        0 19887 1 000000003d97a760 100 0 0 10 0\n"
+        "   3: 00000000:0016 00000000:0000 0A 00000000:00000000 00:00000000 00000000     0        0 17591 1 000000007982095d 100 0 0 10 0\n"
+        "   4: 8D01A8C0:0016 B801A8C0:ECA4 01 00000000:00000000 02:0001C222 00000000     0        0 46224 2 0000000066945424 20 4 19 10 -1\n"
+        "   5: 8D01A8C0:0016 B801A8C0:EC6A 01 0000004C:00000000 01:00000014 00000000     0        0 46152 4 0000000080fea0a4 20 4 29 10 19\n"
+        "   6: 8D01A8C0:0016 B801A8C0:ED34 01 00000000:00000000 02:000291B8 00000000     0        0 51427 2 00000000b4290d70 20 7 31 10 -1\n"
+    )
+
+    ip = _parse_ip_from_tcp_content(tcp_content)
+
+    assert "192.168.1.141" == ip
